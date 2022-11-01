bec6f2bf53851fabf1252ef92a0bfa8da335a2cf
Ion-Osman <ionx0216@edu.easj.dk>
2022-11-01 13:23:44 +0100

    console test

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@ Vue.createApp({
     data() {
         return {
             message: "oaushdoiusahd",
+            output: ""
         }
     },
     
+    async created(){
+        console.log(this.message)
+    },
     methods: {
                  
         
