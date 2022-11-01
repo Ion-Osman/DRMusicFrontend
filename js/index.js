dd4c565093a429889faa1be54e4a77041c35cd5c
SophusS <soph9579@edu.zealand.dk>
2022-11-01 13:31:00 +0100

    Axios integration in JS

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
+baseUri = "";
+
 Vue.createApp({
     data() {
         return {
             message: "oaushdoiusahd",
-            output: ""
+            output: "",
+            error: null
         }
     },
     
     async created(){
         console.log(this.message)
+        this.helperGetPosts(baseUri)
     },
     methods: {
-                 
+             async helperGetPosts(uri){
+                try{
+                    const response = await axois.get(uri)
+                    this.output = await response.data;
+                    this.error = null;
+                } catch(ex){
+                    this.output = "not working";
+                    this.error = ex.message;
+                }
+             }    
         
     }
 }).mount("#app")
